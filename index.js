45e1861f0368544ad9ebac16381cef7073692f49
Erik <erik.langenborg@gmail.com>
2016-11-14 13:50:23 -0500

    Basic operations with a file system database

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Twitter = require('twitter');
 const chalk = require('chalk');
-const env = require('dotenv').config();
+const fs = require('fs');
+const Datastore = require('nedb');
 
+const env = require('dotenv').config();
+const db = new Datastore({ filename: 'tweets.db' });
+
+
+const streamParameters = {
+  locations: '-74,40,-73,41'
+}
 
 const client = new Twitter({
   consumer_key: env.consumer_key,
@@ -11,17 +19,39 @@ const client = new Twitter({
 });
 
 
-client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
-  stream.on('data', function(event) {
-    console.log(event && event.text);
+
+db.loadDatabase((err) => {
+  if (err) {
+    console.error("Unable to work with database", e);
+    return;
+  }
+  console.log(`Database is       [${chalk.green('online')}]`);
+
+
+  client.stream('statuses/filter', streamParameters, (stream) => {
+    stream.on('data', (event) => {
+      console.log(event && event.text);
+      if (!event.coordinates){
+        return;
+      }
+      db.insert(event, ()=>{
+        console.log(`Tweet has been    [${chalk.green('inserted')}]`);
+      });
+    });
+
+    stream.on('error', function(error) {
+      console.log(`Twitter stream is [${chalk.red('offline')}]`);
+      throw error;
+    });
+
+    console.log(`Twitter stream is [${chalk.green('online')}]`);
   });
 
-  stream.on('error', function(error) {
-    throw error;
-  });
 });
+
+
 
 console.log(chalk.blue('================================================================================'));
 console.log('');
 console.log(chalk.blue('================================================================================'));
-console.log(`Twitter stream is [${chalk.green('online')}]`);
+console.log(`Main program is   [${chalk.green('online')}]`);
